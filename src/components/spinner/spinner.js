71aa56ef4686a499a10b31c213520515ec350e94
Евгения Ермакова <russkih.evgeniya92@gmail.com>
2023-01-17 22:17:09 +0400

    add spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+import './spinner.css';
+
+const Spinner = () => (
+    <div className="loadingio-spinner-double-ring-2hw9x2twufe">
+        <div className="ldio-x3rw7hxdh2">
+            <div></div>
+            <div></div>
+            <div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Spinner;
