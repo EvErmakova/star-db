de452f8d7e4b68004cea08811cd799cf7b2fdb12
Евгения Ермакова <russkih.evgeniya92@gmail.com>
2023-01-29 21:02:47 +0400

    add refactoring

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -2,50 +2,30 @@ import React from 'react';
 import ItemList from '../item-list';
 import {withData, withSwapiService} from '../hoc-helpers';
 
-const withChildFunction = (fn) => (Wrapped) => {
-    return (props) => {
-        return (
-            <Wrapped {...props}>
-                {fn}
-            </Wrapped>
-        )
-    }
-};
+const withChildFunction = (fn) => (Wrapped) => (
+    (props) => <Wrapped {...props}>{fn}</Wrapped>
+);
+
+const createList = (props, label) => (
+    withSwapiService(props)(
+        withData(withChildFunction(label)(ItemList))
+    )
+);
+
+const mapMethodsToProps = (data) => ({ getData: data });
 
 const renderPersonLabel = ({ name, gender }) => <span>{name} ({gender})</span>;
 const renderPlanetLabel = ({ name }) => <span>{name}</span>;
 const renderStarshipLabel = ({ name, model }) => <span>{name} ({model})</span>;
 
-const mapPersonMethodsToProps = (swapiService) => {
-    return {
-        getData: swapiService.getAllPeople
-    };
-};
+const mapPersonMethodsToProps = (swapiService) => mapMethodsToProps(swapiService.getAllPeople);
 
-const mapPlanetMethodsToProps = (swapiService) => {
-    return {
-        getData: swapiService.getAllPlanets
-    };
-};
+const mapPlanetMethodsToProps = (swapiService) => mapMethodsToProps(swapiService.getAllPlanets);
 
-const mapStarshipMethodsToProps = (swapiService) => {
-    return {
-        getData: swapiService.getAllStarships
-    };
-};
-
-const createList = (props, label) => {
-    return withSwapiService(props)(
-        withData(withChildFunction(label)(ItemList))
-    );
-};
+const mapStarshipMethodsToProps = (swapiService) => mapMethodsToProps(swapiService.getAllStarships);
 
 const PersonList = createList(mapPersonMethodsToProps, renderPersonLabel);
 const PlanetList = createList(mapPlanetMethodsToProps, renderPlanetLabel);
 const StarshipList = createList(mapStarshipMethodsToProps, renderStarshipLabel);
 
-export {
-    PersonList,
-    PlanetList,
-    StarshipList
-};
+export {PersonList, PlanetList, StarshipList};
